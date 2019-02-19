042e8623d670ba92b89654bc156ebf06888d2dfe
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2019-02-19 14:33:47 +0800

    增加 debug code.

--- src/app/render/sentence/sentence.component.ts
+++ src/app/render/sentence/sentence.component.ts
@@ -33,7 +33,7 @@ export class SentenceComponent implements OnInit, OnDestroy {
   _text: string; // 飛水：天使（%TEXT3%）、聖天馬（%TEXT1%）、吸血蝙蝠（%RTEXT2%）、龍蝦巨獸（%TEXT%）
   _martix: string[]; // ['', '雪莉、安潔莉娜', '', '露娜', '', '索妮亞', '', '安潔莉娜']
 
-  _tokenGroup = this.fb.group({});
+  _tokenGroup = this.fb.group({inputs: new FormArray([])});
 
   _required = false; // 是否所有欄位都是必填狀態。
   _disabled = false; // 是否停用所有 input。
@@ -44,6 +44,7 @@ export class SentenceComponent implements OnInit, OnDestroy {
   ) { }
 
   @Input() set text(val: string) {
+    console.log(`text: ${val}`);
     if (this._text !== val) {
       // 需要 setTimeout 是因為 angular 內部機制衝突，目前這是暫解。
       setTimeout(() => this.apply(val, this._martix)); // 重新產生畫面。
@@ -54,6 +55,7 @@ export class SentenceComponent implements OnInit, OnDestroy {
 
   // 這個屬性也有可能透過 value accessor directive 寫入。
   @Input() set martix(val: string[]) {
+    console.log(`martix: ${JSON.stringify(val)}`);
     if ((this._martix || []).join() !== (val || []).join()) {
       // 需要 setTimeout 是因為 angular 內部機制衝突，目前這是暫解。
       setTimeout(() => this.apply(this._text, val)); // 重新產生畫面。
