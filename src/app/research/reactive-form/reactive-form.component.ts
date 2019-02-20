e333dc4d374ec638f6252dc0ada102df50bf83c8
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2019-02-20 10:36:13 +0800

    新增 reactive form research component.

--- src/app/research/reactive-form/reactive-form.component.ts
+++ src/app/research/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
-  constructor() { }
+  protected name = new FormControl('zoe');
+
+  protected _disabled = false;
+  protected _required = false;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  toggleDisabled() {
+
+    if (this._disabled) {
+      this.name.enable();
+    } else {
+      this.name.disable();
+    }
+
+    this._disabled = !this._disabled;
   }
 
 }
